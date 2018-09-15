893d9723a564780e827e2bdfae4020e62fa1f87e
Sarah Currey <curreysarah28@gmail.com>
2018-09-15 14:39:28 -0400

    pseudocode added

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
+//Predefined variables
 const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"];
 const suits = ["Clubs", "Diamonds", "Hearts", "Spades"];
 const deck = [];
 let player1Card = null;
 let player2Card = null;
 
+//Build Deck Function
 function buildDeck() {
 }
 
+// Deal Cards to Players Function
 function dealCardsToPlayers() {
 }
 
+//Annouce Cards Function
 function announceCards() {
 }
 
+// Card to Rank Function
 function cardToRank(card) {
 }
-
+// Anounce the winner function
 function announceWinner() {
 }
-
+// Return the cards to deck function
 function returnCardsToDeck() {
 }
 
+//Invoking the functions above
 function playGame() {
   dealCardsToPlayers();
   announceCards();
@@ -31,3 +37,6 @@ function playGame() {
 
 buildDeck();
 playGame();
+
+//Keep playing with the while loop
+//Keep score of which player has the highest score. Use alert to show the highest score
